0b12d98def2f2e657f5ebf730692886ddcf8b1d6
uc1015 <91171470+uc1015@users.noreply.github.com>
2021-10-27 12:30:27 +0800

    Add files via upload

---
+++ week07/week07-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n=10;
+    if(n>0){
+        printf("n大於0\n");
+    }
+    while (n>0){
+        printf("n大於0\n");
+    }
+}
---
+++ week07/week07-3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    for( i = 0; i<=3; i++){
+    printf("有幾次呢?\");
+}
+
+    for( int i=0; i<3; i++){
+    printf("老師推薦的寫法,有幾次?\n");
+    }
+}
